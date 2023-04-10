4c5388ca714961334290591b4dfbb045d74cab74
David Batalski <davbot12@gmail.com>
2023-04-10 15:58:26 +0200

    add background, player, sound and animation

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
 import pygame
 
+clock = pygame.time.Clock()
+
 pygame.init()
-screen = pygame.display.set_mode((600, 300)) # , flags=pygame.NOFRAME
+screen = pygame.display.set_mode((618, 359)) # , flags=pygame.NOFRAME
 pygame.display.set_caption("Pygame davbot12 Game")
 icon = pygame.image.load('images/icon.png')
 pygame.display.set_icon(icon)
 
-player = pygame.image.load('images/icon.png')
+bg = pygame.image.load('images/bg.png')
+
+walk_left = [
+    pygame.image.load('images/player_left/player_left1.png'),
+    pygame.image.load('images/player_left/player_left2.png'),
+    pygame.image.load('images/player_left/player_left3.png'),
+    pygame.image.load('images/player_left/player_left4.png'),
+]
+walk_right = [
+    pygame.image.load('images/player_right/player_right1.png'),
+    pygame.image.load('images/player_right/player_right2.png'),
+    pygame.image.load('images/player_right/player_right3.png'),
+    pygame.image.load('images/player_right/player_right4.png'),
+]
+
+player_anim_count = 0
+bg_x = 0
+
+bg_sound = pygame.mixer.Sound('sounds/bg.mp3')
+bg_sound.play()
 
 running = True
 while running:
-    screen.blit(player, (100, 50))
+    screen.blit(bg, (bg_x, 0))
+    screen.blit(bg, (bg_x + 618, 0))
+    screen.blit(walk_right[player_anim_count], (300, 250))
+
+    if player_anim_count == 3:
+        player_anim_count = 0
+    else:
+        player_anim_count += 1
+
+    bg_x -= 2
+    if bg_x == -618:
+        bg_x = 0
 
     pygame.display.update()
 
@@ -19,4 +51,4 @@ while running:
             running = False
             pygame.quit()
 
-
+    clock.tick(10)
