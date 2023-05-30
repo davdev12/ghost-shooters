a195abb1bd09bedb3e3be286199ee003af6c6702
David Batalski <davbot12@gmail.com>
2023-05-30 12:37:00 +0200

    add bullets and shooting ghosts

--- main.py
+++ main.py
@@ -40,59 +40,103 @@ jump_count = 8
 
 
 bg_sound = pygame.mixer.Sound('sounds/bg.mp3')
-bg_sound.play()
+bg_sound.play(loops=-1)
 
 ghost_timer = pygame.USEREVENT +1
 pygame.time.set_timer(ghost_timer, 2500)
+
+label = pygame.font.Font('fonts/Bungee-Regular.ttf', 40)
+lose_label = label.render('You lose!', False, (116, 52, 235))
+restart_label = label.render('Try again', False, (235, 52, 52))
+restart_label_rect = restart_label.get_rect(topleft=(180, 200))
+
+bullets_left = 5
+bullet = pygame.image.load('images/bullet.png').convert_alpha()
+bullets = []
+
+gameplay = True
 
 running = True
 while running:
     screen.blit(bg, (bg_x, 0))
     screen.blit(bg, (bg_x + 618, 0))
 
-    player_rect = walk_left[0].get_rect(topleft=(player_x, player_y))
+    if gameplay:
 
-    if ghost_list_in_game:
-        for el in ghost_list_in_game:
-            screen.blit(ghost, el)
-            el.x -= 10
 
-            if player_rect.colliderect(el):
-                print("You lose!")
+        player_rect = walk_left[0].get_rect(topleft=(player_x, player_y))
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        screen.blit(walk_left[player_anim_count], (player_x, player_y))
-    else:
-        screen.blit(walk_right[player_anim_count], (player_x, player_y))
+        if ghost_list_in_game:
+            for (i, el) in enumerate(ghost_list_in_game):
+                screen.blit(ghost, el)
+                el.x -= 10
 
-    if keys[pygame.K_LEFT] and player_x > 50:
-        player_x -= player_speed
-    elif keys[pygame.K_RIGHT] and player_x < 200:
-        player_x += player_speed
+                if el.x < -10:
+                    ghost_list_in_game.pop(i)
 
-    if not is_jump:
-        if keys[pygame.K_UP]:
-            is_jump = True
-    else:
-        if jump_count >= -8:
-            if jump_count > 0:
-                player_y -= (jump_count ** 2) / 2
-            else:
-                player_y += (jump_count ** 2) / 2
-            jump_count -= 1
+                if player_rect.colliderect(el):
+                    gameplay = False
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            screen.blit(walk_left[player_anim_count], (player_x, player_y))
         else:
-            is_jump = False
-            jump_count = 8
-    if player_anim_count == 3:
-        player_anim_count = 0
+            screen.blit(walk_right[player_anim_count], (player_x, player_y))
+
+        if keys[pygame.K_LEFT] and player_x > 50:
+            player_x -= player_speed
+        elif keys[pygame.K_RIGHT] and player_x < 200:
+            player_x += player_speed
+
+        if not is_jump:
+            if keys[pygame.K_UP]:
+                is_jump = True
+        else:
+            if jump_count >= -8:
+                if jump_count > 0:
+                    player_y -= (jump_count ** 2) / 2
+                else:
+                    player_y += (jump_count ** 2) / 2
+                jump_count -= 1
+            else:
+                is_jump = False
+                jump_count = 8
+        if player_anim_count == 3:
+            player_anim_count = 0
+        else:
+            player_anim_count += 1
+
+        bg_x -= 2
+        if bg_x == -618:
+            bg_x = 0
+
+
+
+        if bullets:
+            for (i, el) in enumerate(bullets):
+                screen.blit(bullet, (el.x, el.y))
+                el.x += 10
+
+                if el.x > 630:
+                    bullets.pop(i)
+
+                if ghost_list_in_game:
+                    for (index, ghost_el) in enumerate(ghost_list_in_game):
+                        if el.colliderect(ghost_el):
+                            ghost_list_in_game.pop(index)
+                            bullets.pop(i)
     else:
-        player_anim_count += 1
+        screen.fill((85, 255, 0))
+        screen.blit(lose_label, (180, 100))
+        screen.blit(restart_label, restart_label_rect)
 
-    bg_x -= 2
-    if bg_x == -618:
-        bg_x = 0
-
+        mouse = pygame.mouse.get_pos()
+        if restart_label_rect.collidepoint(mouse) and pygame.mouse.get_pressed()[0]:
+            gameplay = True
+            player_x = 150
+            ghost_list_in_game.clear()
+            bullets.clear()
+            bullets_left = 5
+        
     pygame.display.update()
 
     for event in pygame.event.get():
@@ -101,5 +145,8 @@ while running:
             pygame.quit()
         if event.type == ghost_timer:
             ghost_list_in_game.append(ghost.get_rect(topleft=(620, 250)))
+        if gameplay and event.type == pygame.KEYUP and event.key == pygame.K_SPACE and bullets_left > 0:
+            bullets.append(bullet.get_rect(topleft=(player_x + 30, player_y + 10)))
+            bullets_left -= 1
 
-    clock.tick(15)
+    clock.tick(20)
