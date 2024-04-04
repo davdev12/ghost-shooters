140558c9345a1775c852d217787965cd7286b4ae
davdev12 <165927613+davdev12@users.noreply.github.com>
2024-04-04 19:15:53 +0200

    Update dev

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ bullet = pygame.image.load('images/bullet.png').convert_alpha()
 bullets = []
 
 counter_label = pygame.font.Font('fonts/Bungee-Regular.ttf', 30)
-hit_counter = 0
+hit_counter = 9999
 
 gameplay = True
 
